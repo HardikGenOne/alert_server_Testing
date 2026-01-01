8801b6bb41744045f4af5ebf24de3e5cab1780bc
HardikGenOne <MaheshwariHardik4@gmail.com>
2026-01-01 15:01:31 +0530

    added cors

--- main.py
+++ main.py
@@ -2,9 +2,15 @@
 
 from fastapi import FastAPI, status
 from fastapi.responses import JSONResponse
-
+from startlette.middleware.cors import CORSMiddleware
 app = FastAPI()
-
+app.add_middleware(
+        CORSMiddleware,
+        allow_origins=["*"],
+        allow_credentials=True,
+        allow_methods=["*"],
+        allow_headers=["*"],
+)
 
 @app.post("/alert")
 def alert(message: str):
