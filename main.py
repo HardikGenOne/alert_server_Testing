8ce127ae4fe41cd8e32a89ca67269a3a589e6864
HardikGenOne <MaheshwariHardik4@gmail.com>
2026-01-01 15:32:20 +0530

    final push

--- main.py
+++ main.py
@@ -2,7 +2,7 @@
 
 from fastapi import FastAPI, status
 from fastapi.responses import JSONResponse
-from startlette.middleware.cors import CORSMiddleware
+from fastapi.middleware.cors import CORSMiddleware
 app = FastAPI()
 app.add_middleware(
         CORSMiddleware,
@@ -12,8 +12,12 @@ app.add_middleware(
         allow_headers=["*"],
 )
 
-@app.post("/alert")
-def alert(message: str):
-    return message
-    # returnSONResponse(status=status.HTTP_200_OK, content= f"message:{message}")
 
+from pydantic import BaseModel
+
+class AlertRequest(BaseModel):
+    req: str
+
+@app.post("/alert")
+async def get_stock_symbol(data: AlertRequest):
+    return {"message": data.req}
